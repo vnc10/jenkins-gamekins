49c49598208c72d2dd10697477afdb49279b18bf
Vinicius Petris <viniciusbosa@gmail.com>
2024-08-18 22:36:52 -0300

    add test

--- src/test/java/com/example/RationalTest.java
+++ src/test/java/com/example/RationalTest.java
@@ -2,7 +2,10 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigInteger;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RationalTest {
 
@@ -11,5 +14,12 @@ public class RationalTest {
         Rational rational1 = Rational.of(3);
         Rational rational2 = Rational.of(3);
         assertEquals(rational2, rational1);
+    }
+
+    @Test
+    void testOf() {
+        BigInteger numerator = BigInteger.ONE;
+        BigInteger denominator = BigInteger.ZERO;
+        assertThrows(IllegalArgumentException.class, () -> Rational.of(numerator, denominator));
     }
 }
