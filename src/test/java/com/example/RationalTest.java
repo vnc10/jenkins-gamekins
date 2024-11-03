7ee02a64a093b70e5a78e752af7bdf078dcecd60
Vinicius Petris <viniciusbosa@gmail.com>
2024-11-03 17:04:06 -0300

    add unit test

--- src/test/java/com/example/RationalTest.java
+++ src/test/java/com/example/RationalTest.java
@@ -15,4 +15,11 @@ public class RationalTest {
         BigInteger denominator = BigInteger.ZERO;
         assertThrows(IllegalArgumentException.class, () -> Rational.of(numerator, denominator));
     }
+
+    @Test
+    void testEquals() {
+        Rational rational1 = Rational.of(3);
+        Rational rational2 = Rational.of(3);
+        assertEquals(rational2, rational1);
+    }
 }
