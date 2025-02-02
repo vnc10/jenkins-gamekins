8cab6976c99e3c9fd20982d0671865f57caff91a
Vinicius Petris <viniciusbosa@gmail.com>
2025-02-02 15:51:48 -0300

    add tests

--- src/test/java/com/example/RationalTest.java
+++ src/test/java/com/example/RationalTest.java
@@ -17,9 +17,23 @@ public class RationalTest {
     }
 
     @Test
+    void testOfWithNewParameters() {
+        BigInteger numerator = new BigInteger(String.valueOf(2));
+        BigInteger denominator = BigInteger.ZERO;
+        assertThrows(IllegalArgumentException.class, () -> Rational.of(numerator, denominator));
+    }
+
+    @Test
     void testEquals() {
         Rational rational1 = Rational.of(3);
         Rational rational2 = Rational.of(3);
+        assertEquals(rational2, rational1);
+    }
+
+    @Test
+    void testEqualsWithDifferentValues() {
+        Rational rational1 = Rational.of(4);
+        Rational rational2 = Rational.of(4);
         assertEquals(rational2, rational1);
     }
 }
