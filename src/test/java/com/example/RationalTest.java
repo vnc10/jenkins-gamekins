8ec070597df59ca240718179abde4e9647b2537c
Vinicius Petris <viniciusbosa@gmail.com>
2025-02-02 15:05:02 -0300

    feature: add new test again

--- src/test/java/com/example/RationalTest.java
+++ src/test/java/com/example/RationalTest.java
@@ -45,6 +45,13 @@ public class RationalTest {
     }
 
     @Test
+    void testEquals2ASDssdd() {
+        Rational rational1 = Rational.of(89);
+        Rational rational2 = Rational.of(89);
+        assertEquals(rational2, rational1);
+    }
+
+    @Test
     void testEquals3() {
         Rational rational1 = Rational.of(3);
         Rational.ofLong(3L);
